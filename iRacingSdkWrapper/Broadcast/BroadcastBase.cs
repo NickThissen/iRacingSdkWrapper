5284a501b3480e53239f6922170c31fd249067ab
NickThissen <nick.thissen@gmail.com>
2015-09-08 20:14:40 +0200

    Initial commit for replay control.

--- iRacingSdkWrapper/Broadcast/BroadcastBase.cs
+++ iRacingSdkWrapper/Broadcast/BroadcastBase.cs
@@ -19,5 +19,11 @@ namespace iRacingSdkWrapper.Broadcast
             if (!_wrapper.IsConnected) return;
             _wrapper.Sdk.BroadcastMessage(type, var1, var2);
         }
+
+        protected void Broadcast(iRSDKSharp.BroadcastMessageTypes type, int var1, int var2, int var3)
+        {
+            if (!_wrapper.IsConnected) return;
+            _wrapper.Sdk.BroadcastMessage(type, var1, var2, var3);
+        }
     }
 }
--- iRacingSdkWrapper/Broadcast/ReplayControl.cs
+++ iRacingSdkWrapper/Broadcast/ReplayControl.cs
@@ -2,27 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using iRSDKSharp;
 
 namespace iRacingSdkWrapper.Broadcast
 {
+    /// <summary>
+    /// Controls the replay system.
+    /// </summary>
     public class ReplayControl : BroadcastBase
     {
         public ReplayControl(SdkWrapper wrapper) : base(wrapper)
         {
         }
 
+        /// <summary>
+        /// Set the replay tape position in number of frames since the session start.
+        /// </summary>
+        /// <param name="frameFromBeginning"></param>
         public void SetPosition(int frameFromBeginning)
         {
-            Broadcast(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlayPosition,
-                (int)iRSDKSharp.ReplayPositionModeTypes.Begin,
+            Broadcast(BroadcastMessageTypes.ReplaySetPlayPosition,
+                (int)ReplayPositionModeTypes.Begin,
                 frameFromBeginning);
         }
 
+        /// <summary>
+        /// Set the replay tape position in number of frames from the session end.
+        /// </summary>
+        /// <param name="frameFromEnd"></param>
         public void SetPositionFromEnd(int frameFromEnd)
         {
-            Broadcast(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlayPosition,
-                (int)iRSDKSharp.ReplayPositionModeTypes.End,
+            Broadcast(BroadcastMessageTypes.ReplaySetPlayPosition,
+                (int)ReplayPositionModeTypes.End,
                 frameFromEnd);
+        }
+
+        /// <summary>
+        /// Jump to live.
+        /// </summary>
+        public void JumpToLive()
+        {
+            Jump(ReplaySearchModeTypes.ToEnd);
+        }
+
+        /// <summary>
+        /// Jump to the start.
+        /// </summary>
+        public void JumpToStart()
+        {
+            Jump(ReplaySearchModeTypes.ToStart);
+        }
+
+        /// <summary>
+        /// Jump to a specific event.
+        /// </summary>
+        /// <param name="replayEvent">The event to jump to.</param>
+        public void Jump(ReplaySearchModeTypes replayEvent)
+        {
+            Broadcast(BroadcastMessageTypes.ReplaySearch,
+                (int)replayEvent, 0);
         }
     }
 }
